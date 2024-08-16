9c7aaf1ec56884c101fc66fb5f872e6043b839cf
montleee <144028935+montleee@users.noreply.github.com>
2024-08-17 00:54:06 +0300

    init

---
+++ src/main/java/com/meowmentor/themeservice/ThemeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.meowmentor.themeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThemeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThemeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meowmentor/themeservice/ThemeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meowmentor.themeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThemeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
